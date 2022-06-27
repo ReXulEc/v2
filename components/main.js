48d2471fe73625c80e4b21a9ed47f121c281e5d5
Mert Doğu <mertdoguonder@protonmail.com>
2022-06-27 16:24:49 +0300

    change: font weight

--- components/main.js
+++ components/main.js
@@ -7,7 +7,7 @@ export default function Main() {
       >
         <div className="text-4xl">
             <p className=" font-bold text-white/90">Mert Doğu</p>
-            <p className=" text-lg font-thin text-white/50">My name is Mert Doğu and welcome to my profile. Im the founder of settleco.net and co-founder of mergeapp.dev. I'm currently doing some projects for Settleco and having a lot of fun while doing it. For more information about me, you can view my personal page at rexulec.com</p>
+            <p className=" text-lg font-semibold text-white/50">My name is Mert Doğu and welcome to my profile. Im the founder of settleco.net and co-founder of mergeapp.dev. I'm currently doing some projects for Settleco and having a lot of fun while doing it. For more information about me, you can view my personal page at rexulec.com</p>
         </div>
       </div>
     )
