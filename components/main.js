d7866998450d0a3069b79c773edfa8dbed629779
Mert Doğu <mertdoguonder@protonmail.com>
2022-07-05 17:47:08 +0300

    change:text

--- components/main.js
+++ components/main.js
@@ -7,7 +7,7 @@ export default function Main() {
       >
         <div className="text-4xl">
             <p className=" font-bold text-white/90">Mert Doğu</p>
-            <p className=" text-lg font-medium text-white/50">My name is Mert Doğu and welcome to my profile. Im the founder of settleco.net and co-founder of mergeapp.dev. I'm currently doing some projects for Settleco and having a lot of fun while doing it. For more information about me, you can view my personal page at rexulec.com</p>
+            <p className=" text-lg font-medium text-white/50">My name is Mert Doğu and welcome to my profile. Im the founder of settleco.net and co-founder of mergeapp.dev. I'm currently doing some projects for Settleco and having a lot of fun while doing it. For more information about me, you can find my socials below! </p>
         </div>
       </div>
     )
